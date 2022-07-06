a83475adf8a6e1bc59a404318b7078e959ef131a
박성훈 / Koder <koder0205@gmail.com>
2022-07-06 20:39:41 +0900

    맥os 로 변경하면서 커밋

--- index.js
+++ index.js
@@ -3,9 +3,16 @@
 var test = navigator.userAgent;
 //document.writeln(test);
 
-var UserAgent = navigator.userAgent.match(/\((.*?)\)/gi)[0];
-var ModelName = UserAgent.replace(/[()]/gi, "").split(';')[2];
-document.writeln(ModelName);
+var test2 = navigator.userAgent.getHighEntropyValues(["model"]).then(ua => {document.writeln(ua)});
+
+document.writeln(test2);
+/*
+var UserAgent = navigator.userAgent;
+var Device    = UserAgent.match(/\((.*?)\)/gi);
+var ModelName = Device[0].replace(/[()]/gi, "").split(';')[2];
+document.writeln(UserAgent);
 
 var root = document.querySelector(':root');
 root.style.setProperty('--roundval', '15px');
+
+*/
